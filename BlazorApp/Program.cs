c219cedcf8b6d705ee87596388ebcec04bb2675d
Guilherme Andrade <guilhermevmandrade@gmail.com>
2025-03-30 22:14:36 -0300

    feat: add blazor app que faz requisição ao appi gateway

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,13 @@
+using BlazorApp;
+using BlazorApp.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") });
+builder.Services.AddScoped<ApiService>();
+
+await builder.Build().RunAsync();
---
+++ BlazorApp/Services/ApiService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorApp.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "http://localhost:5000";
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<T>> GetAsync<T>(string endpoint)
+        {
+            var response =  await _httpClient.GetFromJsonAsync<List<T>>($"{BaseUrl}/{endpoint}");
+            if (response == null) 
+            { 
+                return new List<T>();
+            }
+            return response;
+        }
+    }
+}
